cba9b689b64ac232c303f77e07d28763a7b4dff9
nem035 <n.stojanovic035@gmail.com>
2016-08-11 23:39:16 -0400

    Thu Aug 11 23:39:16 EDT 2016

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -2,6 +2,9 @@ const React = require('react');
 const Clock = require('Clock');
 const CountdownForm = require('CountdownForm');
 
+const STOPPED = 'stopped';
+const STARTED = 'started';
+
 class Countdown extends React.Component {
 
   constructor(props) {
@@ -11,12 +14,47 @@ class Countdown extends React.Component {
 
     this.state = {
       seconds: 0,
+      status: STOPPED,
     };
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const newStatus = this.state.status;
+    const oldStatus = prevState.status;
+
+    if (newStatus !== oldStatus) {
+      switch (newStatus) {
+        case STARTED:
+          this.startTimer();
+          break;
+        case STOPPED:
+          this.stopTimer();
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      const { state } = this;
+      const seconds = state.seconds > 0 ? state.seconds - 1 : 0;
+      this.setState({
+        seconds,
+      });
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+  }
+
   handleSetCountdown(seconds) {
+    const status = STARTED;
     this.setState({
       seconds,
+      status,
     });
   }
 
